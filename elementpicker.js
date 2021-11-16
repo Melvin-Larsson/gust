0ff4e35c84e0670245ad36f880b04861911420e8
Melvin-Larsson <Melvin.Larsson@Hotmail.se>
2021-11-16 21:43:09 +0100

    Added lookup of elements similar to the clicked

--- elementpicker.js
+++ elementpicker.js
@@ -9,27 +9,50 @@ pickElement.addEventListener("click", async() => {
         target: { tabId: tab.id },
         function: setupListener, 
     });
-    console.log("Something happened");
 
-    function setupListener(){
-        document.body.addEventListener('mousemove', e => {
-            //Remove last overlay
-            let currentOverlay = document.body.querySelector('#hover');
-            if(currentOverlay){
-                document.body.removeChild(currentOverlay);
-            }
-            //Add overlay
-            let element = document.elementFromPoint(e.x, e.y);
-            let boundingRect = element.getBoundingClientRect();
-            let overlay = document.createElement("div");
-            overlay.id = "hover";
-            overlay.style.position = "absolute";
-            overlay.style.top = boundingRect.y;
-            overlay.style.left = boundingRect.x;
-            overlay.style.width = boundingRect.width;
-            overlay.style.height = boundingRect.height;
-            overlay.style.backgroundColor = "rgba(72,159,240,0.5)";
-            document.body.appendChild(overlay);
-        });
-    }
 });
+function setupListener(){
+    //Mouse move listener
+    document.body.addEventListener('mousemove', e => {
+        //Remove last overlay
+        clearOverlay();
+        //Add overlay
+        let element = document.elementFromPoint(e.x, e.y);
+        let boundingRect = element.getBoundingClientRect();
+        let overlay = document.createElement("div");
+        //Style
+        overlay.id = "hover";
+        overlay.style.position = "absolute";
+        overlay.style.top = boundingRect.y;
+        overlay.style.left = boundingRect.x;
+        overlay.style.width = boundingRect.width;
+        overlay.style.height = boundingRect.height;
+        overlay.style.backgroundColor = "rgba(72,159,240,0.5)";
+        document.body.appendChild(overlay);
+    });
+    //Mouse pressed listener
+    document.body.addEventListener('click', e =>{
+        //Remove last overlay
+        clearOverlay();
+        let element = document.elementFromPoint(e.x, e.y);
+        let tag = element.tagName.toLowerCase();
+        let classList = element.classList;
+        let selector = "";
+        if(classList.length > 0){
+            classList.forEach(c => {
+                selector += tag + "." + c;
+            });
+        }else{
+            selector = tag;
+        }
+        let selectedElements = document.querySelectorAll(selector);
+        console.log(selectedElements);
+    });
+    //Removes the last overlay
+    function clearOverlay(){
+        let currentOverlay = document.body.querySelector('#hover');
+        if(currentOverlay){
+            document.body.removeChild(currentOverlay);
+        }
+    }
+}

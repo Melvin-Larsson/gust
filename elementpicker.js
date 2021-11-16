c57ec3679a3af4784660c7f095fd038362be5ffa
Melvin-Larsson <Melvin.Larsson@Hotmail.se>
2021-11-16 21:52:25 +0100

    Added overlay to elements similar to the clicked

--- elementpicker.js
+++ elementpicker.js
@@ -15,25 +15,16 @@ function setupListener(){
     //Mouse move listener
     document.body.addEventListener('mousemove', e => {
         //Remove last overlay
-        clearOverlay();
+        removeOverlay("hover");
         //Add overlay
         let element = document.elementFromPoint(e.x, e.y);
-        let boundingRect = element.getBoundingClientRect();
-        let overlay = document.createElement("div");
-        //Style
-        overlay.id = "hover";
-        overlay.style.position = "absolute";
-        overlay.style.top = boundingRect.y;
-        overlay.style.left = boundingRect.x;
-        overlay.style.width = boundingRect.width;
-        overlay.style.height = boundingRect.height;
-        overlay.style.backgroundColor = "rgba(72,159,240,0.5)";
-        document.body.appendChild(overlay);
+        createOverlay(element, "hover");
     });
     //Mouse pressed listener
     document.body.addEventListener('click', e =>{
         //Remove last overlay
-        clearOverlay();
+        removeOverlay("hover");
+        //Query for elements similar to the clicked
         let element = document.elementFromPoint(e.x, e.y);
         let tag = element.tagName.toLowerCase();
         let classList = element.classList;
@@ -46,13 +37,29 @@ function setupListener(){
             selector = tag;
         }
         let selectedElements = document.querySelectorAll(selector);
-        console.log(selectedElements);
+        //Put overlay above the similar elements
+        selectedElements.forEach(element => {
+            createOverlay(element, "element", "rgba(247, 250, 67, 0.5)");
+        });
     });
     //Removes the last overlay
-    function clearOverlay(){
-        let currentOverlay = document.body.querySelector('#hover');
+    function removeOverlay(className){
+        let currentOverlay = document.body.querySelector(`.${className}`);
         if(currentOverlay){
             document.body.removeChild(currentOverlay);
         }
+    }
+    function createOverlay(element, className, colorString = "rgba(72, 159, 240, 0.5)"){
+        let boundingRect = element.getBoundingClientRect();
+        let overlay = document.createElement("div");
+        //Style
+        overlay.className = className;
+        overlay.style.position = "absolute";
+        overlay.style.top = boundingRect.y;
+        overlay.style.left = boundingRect.x;
+        overlay.style.width = boundingRect.width;
+        overlay.style.height = boundingRect.height;
+        overlay.style.backgroundColor = colorString;
+        document.body.appendChild(overlay);
     }
 }

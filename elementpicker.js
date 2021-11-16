cb5c01b7861989940f571ea0bc93f012f74ffc72
Melvin-Larsson <Melvin.Larsson@Hotmail.se>
2021-11-16 21:12:13 +0100

    Added overlay above element below cursor

--- elementpicker.js
+++ elementpicker.js
@@ -1,4 +1,6 @@
 let pickElement = document.getElementById("pickElementButton");
+let hoverId = "hover";
+let lastelement;
 
 pickElement.addEventListener("click", async() => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -11,7 +13,23 @@ pickElement.addEventListener("click", async() => {
 
     function setupListener(){
         document.body.addEventListener('mousemove', e => {
-            console.log(e.x + " " + e.y);
+            //Remove last overlay
+            let currentOverlay = document.body.querySelector('#hover');
+            if(currentOverlay){
+                document.body.removeChild(currentOverlay);
+            }
+            //Add overlay
+            let element = document.elementFromPoint(e.x, e.y);
+            let boundingRect = element.getBoundingClientRect();
+            let overlay = document.createElement("div");
+            overlay.id = "hover";
+            overlay.style.position = "absolute";
+            overlay.style.top = boundingRect.y;
+            overlay.style.left = boundingRect.x;
+            overlay.style.width = boundingRect.width;
+            overlay.style.height = boundingRect.height;
+            overlay.style.backgroundColor = "rgba(72,159,240,0.5)";
+            document.body.appendChild(overlay);
         });
     }
 });
